68260b8f49621c8aeb4ae1c44ace406722699d7c
enzgamilla <laurenceangelogamilla@gmail.com>
2024-08-04 09:27:35 +0800

    sort table using url parameter and fast sort

--- app/users/new/UserTable.tsx
+++ app/users/new/UserTable.tsx
@@ -1,3 +1,5 @@
+import { sort } from "fast-sort";
+import Link from "next/link";
 import React from "react";
 
 interface User {
@@ -6,23 +8,35 @@ interface User {
   email: string;
 }
 
-const UserTable = async () => {
+interface SortProps {
+  sortBy: string;
+}
+
+const UserTable = async ({ sortBy }: SortProps) => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users", {
     next: { revalidate: 10 },
   }); //with cache
+
   const users: User[] = await res.json();
+  const sorted = sort(users).asc(
+    sortBy === "name" ? (user) => user.name : (user) => user.email
+  );
 
   return (
     <>
       <table className="table table-pin-rows">
         <thead>
           <tr>
-            <th>Name</th>
-            <th>Email</th>
+            <th>
+              <Link href="/users?sortOrder=name">Name</Link>
+            </th>
+            <th>
+              <Link href="/users?sortOrder=email">Email</Link>
+            </th>
           </tr>
         </thead>
         <tbody>
-          {users.map((user) => (
+          {sorted.map((user) => (
             <tr key={user.id}>
               <td>{user.name}</td>
               <td>{user.email}</td>
