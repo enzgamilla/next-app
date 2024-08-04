c79fac6e513b72eff876aa99472e062d95e01dbd
enzgamilla <laurenceangelogamilla@gmail.com>
2024-08-04 14:39:16 +0800

    programmatic navigation

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import UserTable from "./new/UserTable";
+import Link from "next/link";
 
 interface Props {
   searchParams: { sortOrder: string };
@@ -9,6 +10,9 @@ const UserPage = async ({ searchParams: { sortOrder } }: Props) => {
   return (
     <div className="p-3 rounded-xl">
       <h1>Users</h1>
+      <Link href="users/new" className="btn">
+        New User
+      </Link>
       <UserTable sortBy={sortOrder} />
     </div>
   );
