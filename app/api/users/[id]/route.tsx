80007ba6d6862ccb120f1bd3abe7235e8871c988
enzgamilla <laurenceangelogamilla@gmail.com>
2024-08-08 15:16:54 +0800

    update and delete users in database

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -25,9 +25,41 @@ export async function PUT(request: NextRequest, { params: { id } }: Props) {
   if (!validation.success)
     return NextResponse.json(validation.error.errors, { status: 400 });
 
-  return NextResponse.json({ id: 1, name: body.name });
+  const getUser = await prisma.user.findUnique({
+    where: {
+      id: parseInt(id),
+    },
+  });
+  if (!getUser)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+
+  const updatedUser = await prisma.user.update({
+    where: {
+      id: parseInt(id),
+    },
+    data: {
+      name: body.name,
+      email: body.email,
+    },
+  });
+
+  return NextResponse.json(updatedUser);
 }
 
 export async function DELETE(request: NextRequest, { params: { id } }: Props) {
+  const getUser = await prisma.user.findUnique({
+    where: {
+      id: parseInt(id),
+    },
+  });
+  if (!getUser)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+
+  await prisma.user.delete({
+    where: {
+      id: parseInt(id),
+    },
+  });
+
   return NextResponse.json({});
 }
