6cdb5bef9e5b4482b9c21acb9543d1272860507d
enzgamilla <laurenceangelogamilla@gmail.com>
2024-08-09 15:51:46 +0800

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,19 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+// import { NextResponse } from "next/server";
+// import type { NextRequest } from "next/server";
+// import { default } from 'next-auth/middleware'
+import middleware from "next-auth/middleware";
 
 // This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL("/new-page", request.url));
-}
+// export function middleware(request: NextRequest) {
+//   return NextResponse.redirect(new URL("/new-page", request.url));
+// }
+
+export default middleware;
 
 // See "Matching Paths" below to learn more
 export const config = {
-  //*: one or more
-  //+: zero or more
-  //?: one or zero
-  matcher: "/users/:path*",
+  //*: zero or more
+  //+: one or more
+  //?: zero or one
+  matcher: "/dashboard/:path*",
 };
