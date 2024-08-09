7f190e0c788171191547f1f73bbeb2d7f82b8159
enzgamilla <laurenceangelogamilla@gmail.com>
2024-08-09 14:09:59 +0800

    login using google account

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,7 +1,11 @@
+"use client";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import React from "react";
 
 const NavBar = () => {
+  const { status, data: session } = useSession();
+
   return (
     <div className="flex navbar-center bg-slate-200 p-5">
       <Link href="/" className="mr-5">
@@ -16,9 +20,13 @@ const NavBar = () => {
       <Link href="/users" className="mr-5">
         User
       </Link>
-      <Link href="/api/auth/signin" className="mr-5">
-        Login
-      </Link>
+      {status === "loading" && <div>Loading...</div>}
+      {status === "authenticated" && <div>{session.user!.name}</div>}
+      {status === "unauthenticated" && (
+        <Link href="/api/auth/signin" className="mr-5">
+          Login
+        </Link>
+      )}
     </div>
   );
 };
